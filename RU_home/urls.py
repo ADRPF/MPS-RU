eb491bf988fd631330da8fcb0b4205aa16d91292
Amós do Carmo Moreira Filho <adcmf.snf20@uea.edu.br>
2023-08-08 23:37:49 -0400

    Sistema de Cadastro e Login

--- RU_home/urls.py
+++ RU_home/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('cardapio/', views.ver_cardapio, name="ver_cardapio"),
-    path('pedidos/', views.ver_pedidos, name="ver_pedidos")
+    path('cadastro/', views.cadastrar_aluno, name="cadastrar_aluno"),
+    path('login/', views.logar, name="logar"),
+    path('inicio/', views.index, name="inicio")
 ]
--- RU_home/views.py
+++ RU_home/views.py
@@ -5,22 +5,40 @@ from .models import Pessoa
 def ver_cardapio(request):
     return HttpResponse('Estou no Cardápio')
 
-def ver_pedidos(request):
+def logar(request):
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        senha = request.POST.get('senha')
+        print(nome, senha)
+        pessoa = Pessoa.objects.filter(nome=nome, senha=senha)
+        if pessoa.exists():
+            print("Usuário Cadastrado")
+            return render(request, 'index.html')
+        else:
+            print("Não existe esse usuário")
+    return render(request, 'pagina_login.html')
+
+def index(request):
+    return render(request, 'index.html')
+
+def cadastrar_aluno(request):
     if request.method == "GET":
         nome = 'Amós'
-        return render(request, 'ver_pedido.html', {'nome':nome})
+        return render(request, 'pagina_cadastro.html', {'nome':nome})
     elif request.method == "POST":
         nome = request.POST.get('nome')
         matricula = request.POST.get('matricula')
-        pessoa = Pessoa(nome=nome, matricula=matricula)
+        senha = request.POST.get('senha')
+        pessoa = Pessoa(nome=nome, matricula=matricula, senha=senha)
         pessoas = Pessoa.objects.all()
         pessoas2 = Pessoa.objects.filter(nome=nome)
         if pessoas2.exists():
             print("Usuário já cadastrado")
         else:
             print("Usuário cadastrado")
-        print(pessoas[0].idade)
+        print(pessoas[0].matricula)
         #print(pessoas)
         pessoa.save()
-        return HttpResponse(pessoas)
+        #return HttpResponse(pessoas)
+        return render(request, 'pagina_login.html')
 
